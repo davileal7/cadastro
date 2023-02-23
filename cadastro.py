016b80f22b2517888e108a8f8fcfdc6e2e9bd555
davileal7 <108898208+davileal7@users.noreply.github.com>
2023-02-23 15:16:26 -0300

    adicionei email

--- cadastro.py
+++ cadastro.py
@@ -1,7 +1,9 @@
 nomes = []
+email = []
 print('Seja bem-vindo ao programa de cadastro!')
 while True:
     nomes.append(str(input(f'Cadastre um nome: ')).capitalize())
+    email.append(str(input(f'digite seu email: ')))
     resposta = str(input("Quer continuar? (S/N) ")).upper()
     while resposta not in 'S''N':
         print('\033[31mERRO!!!\033[m')
@@ -10,7 +12,9 @@ while True:
         break
 print(f'Ao todo tivemos {len(nomes)} nomes cadastrados.')
 for ordem, nome in enumerate(nomes):
-    print(f'\033[34m{nome}\033[m é o {ordem + 1}° nome.')
+    print(f'{ordem + 1}° \033[34m{nome}\033[m.')
+for ordem, mail in enumerate(email):
+    print(f'{ordem + 1}°email \033[33m{mail}\033[m')
 print('Obrigado por utilizar este programa.')
 print('Até a próxima!')
 
